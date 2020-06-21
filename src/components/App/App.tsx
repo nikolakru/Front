7da57a60aa24947a45f6cc8eb39ad91064e2dd23
Nikola Krunic <dzony.kurnic@gmail.com>
2020-06-21 02:46:46 +0200

    Bootstrap added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import './App.css';
-
+import {Container} from 'react-bootstrap';
+import {faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container >
+          <FontAwesomeIcon icon={ faHome } /> Home
+    </Container>
   );
 }
 
